b896047721c6e0b5d958544f7b42847cbaf38ed8
Subramanyam1995 <subramanyam04111995@gmail.com>
2024-06-12 12:48:11 +0530

    1

--- Producation_CRM/Producation_CRM_2.py
+++ Producation_CRM/Producation_CRM_2.py
@@ -1,6 +1,6 @@
 
 from Producation_CRM.Producation_CRM_3_Module_Imports import *
-from Producation_CRM_2_SideFiles.Grid_0 import *
+from Producation_CRM_2_SideFiles.Grid_0 import grid_0
 
 
 
@@ -14,51 +14,6 @@ def disabler_window_1_1_0(value,lab):
         for i in value.winfo_children():
             i.configure(state="normal") 
 
-def Pusher_grid_0():
-    window_pusher = tk.Tk()
-    tk.Label(window_pusher,text = "Heading",font='Bahnschrift 20 bold').pack()
-    window_pusher.mainloop()
-
-
-def grid_0(window,value_inputed):
-    
-
-    window_01_01_01 = tk.Frame(window)
-    
-    # Heading Name Stage_1//2
-    Heading = tk.Label(window_01_01_01, text="Heading",font='Bahnschrift 20 bold', command=None)
-    Heading.grid(row=0,column=0,columnspan=2)    
-    
-    
-    # Stage 01_01
-    # Key : Labels 
-    
-    Old_Serial_Name = tk.StringVar(value=f"{value_inputed}___01_Old_Serial_Name")
-    Old_Serial_Name_010101 = tk.Label(window_01_01_01, text=Old_Serial_Name.get(),font='Bahnschrift 20 bold')
-    Old_Serial_Name_010101.grid(row=1,column=0,columnspan=2) 
-
-    Name = tk.StringVar(value=f"{value_inputed}___010101__Name")
-    tk.Label(window_01_01_01, text= Name.get(),font='Bahnschrift 12 bold', command=None).grid(row=2,column=0)
-
-    OfficeName = tk.StringVar(value=f"{value_inputed}___01021__OfficeName")
-    tk.Label(window_01_01_01, text=OfficeName.get(),font='Bahnschrift 12 bold', command=None).grid(row=3,column=0)
-    
-    # Value : Entrys
-
-    Name__010101 = tk.StringVar(window_01_01_01)
-    tk.Entry(window_01_01_01, textvariable=Name__010101, font='Bahnschrift 12 bold').grid(row=2,column=1)  
-
-    OfficeName__01021 = tk.StringVar(window_01_01_01)
-    tk.Entry(window_01_01_01, textvariable=OfficeName__01021, font='Bahnschrift 12 bold').grid(row=3,column=1)    
-
-
-    tk.Button(window_01_01_01, text="Pusher", font='Bahnschrift 12 bold', command=Pusher_grid_0).grid(row=7,columnspan=2,column=0)  
-    
-   
-    Disabler_1 = tk.Checkbutton(window, text= "Disabler", command=lambda : disabler_window_1_1_0(window_01_01_01,Heading))
-    Disabler_1.pack(anchor="ne",side="top")
-
-    window_01_01_01.pack()
 
 # Secific Funcation
 
@@ -66,9 +21,6 @@ def grid_0(window,value_inputed):
 
 def Producation_1_1_0(value_inputed):
 
-
-    DataBase = {}
-    value_inputed = 100
     window_1_1_0 = tk.Tk()
 
     window_1_1_0.geometry("500x700")
--- Producation_CRM/Producation_CRM_3_Module_Imports.py
+++ Producation_CRM/Producation_CRM_3_Module_Imports.py
@@ -8,24 +8,26 @@ import json
 import time
 from pprint import pprint
 
-list1 = ['01_Old_Name',
- '02_Owner_Names',
- '03_Location',
- '04_Visiting_Card_Pic',       
- '05_Fraud',
- '06_Personal_Data',
- '07_Sales_Report',
- '08_Recording',
- '09_Customer_Links',
- '10_Anydesk_Deatils',
- '11_FollowUps_Request',       
- '12_Customer_Update_Required',
- '13_Gold_Mine',
- '14_Date_of_Database_Creation',
- '15_Database_Deactiver',
- '16_Caution']
+# Funcation To All
 
+Set_of_Printing_Company = {
+                            "Keshav": {"Registed_date":None
+                                       
+                                       },
+                            "BOP":{"Registed_date":None},
+                            
 
+                            }
+
+# Funcation To All
+
+def disabler_window_1_1_0(value,lab):
+    if lab["state"] == "normal" : 
+        for i in value.winfo_children():
+            i.configure(state="disable") 
+    else: 
+        for i in value.winfo_children():
+            i.configure(state="normal") 
 
 
 

a90548e28325544fe0f1c113d3a3ad9816e7102c
Subramanyam1995 <subramanyam04111995@gmail.com>
2024-06-15 16:47:05 +0530

    1

--- 09_Test Cases/Test__python.py
+++ 09_Test Cases/Test__python.py
@@ -9,23 +9,3 @@ import datetime
 
 
 
-window = tk.Tk()
-
-window.title("CRM_1")
-
-def Main_selection_page():
-
-
-    Button_destroy = ctk.CTkButton(window, text="Destroy" , command=lambda : window.destroy())
-    Button_destroy.pack()
-
-
-
-window.geometry("800x900")
-
-Button = ctk.CTkButton(window, text="call_one", command=Main_selection_page)
-Button.pack()
-
-window.mainloop()
-
-
